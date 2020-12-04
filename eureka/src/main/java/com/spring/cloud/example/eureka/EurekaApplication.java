34d5f87da64481f3590d10635799afe144e2ea3c
wudi <291100854@qq.com>
2020-12-04 18:19:59 +0800

    add pom 加配置文件

--- eureka/src/main/java/com/spring/cloud/example/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/spring/cloud/example/eureka/EurekaApplication.java
@@ -2,12 +2,15 @@ package com.spring.cloud.example.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author aaron
  */
 @EnableEurekaServer
+//开启作为Eureka Server的客户端的支持
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaApplication {
 
