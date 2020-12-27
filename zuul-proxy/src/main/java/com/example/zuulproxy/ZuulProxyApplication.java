1cbe261783f2dec13404fa213f5fe3594e2578d1
wudi <291100854@qq.com>
2020-12-27 14:50:10 +0800

    add zuul

---
+++ zuul-proxy/src/main/java/com/example/zuulproxy/ZuulProxyApplication.java
@@ -0,0 +1,20 @@
+package com.example.zuulproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author aaron
+ */
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZuulProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulProxyApplication.class, args);
+    }
+
+}
