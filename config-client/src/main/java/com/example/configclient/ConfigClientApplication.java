aa296fbceba0868adaba82dad68f8a73bd3eb99e
wudi <291100854@qq.com>
2020-12-27 17:38:28 +0800

    配置中心

---
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -0,0 +1,17 @@
+package com.example.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author aaron
+ */
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
