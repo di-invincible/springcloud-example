56e8318becdfe53bdd8c243a5d35d375923f5d98
wudi <291100854@qq.com>
2021-01-08 19:30:55 +0800

    Oauth2 单点登录 (oauth2-client添加权限校验没做)

--- oauth2-jwt-server/src/main/java/com/example/oauth2jwtserver/config/AuthorizationServerConfig.java
+++ oauth2-jwt-server/src/main/java/com/example/oauth2jwtserver/config/AuthorizationServerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -57,12 +58,14 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
         TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
         List<TokenEnhancer> delegates = new ArrayList<>();
-        delegates.add(jwtTokenEnhancer); //配置JWT的内容增强器
+        //配置JWT的内容增强器
+        delegates.add(jwtTokenEnhancer);
         delegates.add(jwtAccessTokenConverter);
         enhancerChain.setTokenEnhancers(delegates);
         endpoints.authenticationManager(authenticationManager)
                 .userDetailsService(userService)
-                .tokenStore(tokenStore) //配置令牌存储策略
+                //配置令牌存储策略
+                .tokenStore(tokenStore)
                 .accessTokenConverter(jwtAccessTokenConverter)
                 .tokenEnhancer(enhancerChain);
     }
@@ -71,12 +74,27 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("admin")//配置client_id
-                .secret(passwordEncoder.encode("admin123456"))//配置client_secret
-                .accessTokenValiditySeconds(3600)//配置访问token的有效期
-                .refreshTokenValiditySeconds(864000)//配置刷新token的有效期
-                .redirectUris("http://localhost:9501/login")//配置redirect_uri，用于授权成功后跳转
-                .scopes("all")//配置申请的权限范围
-                .authorizedGrantTypes("authorization_code","password","refresh_token");//配置grant_type，表示授权类型
+                //配置client_id
+                .withClient("admin")
+                //配置client_secret
+                .secret(passwordEncoder.encode("admin123456"))
+                //配置访问token的有效期
+                .accessTokenValiditySeconds(3600)
+                //配置刷新token的有效期
+                .refreshTokenValiditySeconds(864000)
+                //配置redirect_uri，用于授权成功后跳转
+                .redirectUris("http://localhost:9501/login")
+                //自动授权配置
+                .autoApprove(true)
+                //配置申请的权限范围
+                .scopes("all")
+                //配置grant_type，表示授权类型
+                .authorizedGrantTypes("authorization_code","password","refresh_token");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) {
+        // 获取密钥需要身份认证，使用单点登录时必须配置
+        security.tokenKeyAccess("isAuthenticated()");
     }
 }
